6fd37bcaadc8ba9406b60075dc1d0d516fcf0db1
notghettolenny <79231993+notghettolenny@users.noreply.github.com>
2022-06-03 00:08:09 +0100

    Add files via upload

---
+++ 3_Lab_Autoencoders/AUTOENCODERS.py
@@ -0,0 +1,114 @@
+#AUTOENCODERS
+#Let's call our imports and make the MNIST data available to use.
+#from __future__ import division, print_function, absolute_import
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+# Import MINST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+#Now, let's give the parameters that are going to be used by our NN.
+learning_rate = 0.01
+training_epochs = 20
+batch_size = 256
+display_step = 1
+examples_to_show = 10
+
+# Network Parameters
+n_hidden_1 = 256 # 1st layer num features
+n_hidden_2 = 128 # 2nd layer num features
+n_input = 784 # MNIST data input (img shape: 28*28)
+
+# tf Graph input (only pictures)
+X = tf.placeholder("float", [None, n_input])
+
+weights = {
+    'encoder_h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'encoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'decoder_h1': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_1])),
+    'decoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_input])),
+}
+biases = {
+    'encoder_b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'encoder_b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'decoder_b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'decoder_b2': tf.Variable(tf.random_normal([n_input])),
+}
+
+'''
+Now we need to create our encoder. For this, we are going to use sigmoidal functions. 
+Sigmoidal functions delivers great results with this type of network. This is due to having a good derivative that is well-suited to backpropagation. 
+We can create our encoder using the sigmoidal function like this:
+'''
+# Building the encoder
+def encoder(x):
+    # Encoder first layer with sigmoid activation #1
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']), biases['encoder_b1']))
+    # Encoder second layer with sigmoid activation #2
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']), biases['encoder_b2']))
+    return layer_2
+
+#And the decoder:
+#You can see that the layer_1 in the encoder is the layer_2 in the decoder and vice-versa.
+# Building the decoder
+def decoder(x):
+    # Decoder first layer with sigmoid activation #1
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']),biases['decoder_b1']))
+    # Decoder second layer with sigmoid activation #2
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']), biases['decoder_b2']))
+    return layer_2
+
+#Let's construct our model. In the variable cost we have the loss function and in the optimizer variable we have our gradient used for backpropagation.-----
+# Construct model
+encoder_op = encoder(X)
+decoder_op = decoder(encoder_op)
+
+# Reconstructed Images
+y_pred = decoder_op
+# Targets (Labels) are the input data.
+y_true = X
+
+# Define loss and optimizer, minimize the squared error
+cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
+optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+#For training we will run for 20 epochs.--------------------------
+# Launch the graph
+# Using InteractiveSession (more convenient while using Notebooks)
+sess = tf.InteractiveSession()
+sess.run(init)
+
+total_batch = int(mnist.train.num_examples / batch_size)
+# Training cycle
+for epoch in range(training_epochs):
+    # Loop over all batches
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        # Run optimization op (backprop) and cost op (to get loss value)
+        _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs})
+    # Display logs per epoch step
+    if epoch % display_step == 0:
+        print("Epoch:", '%04d' % (epoch+1),
+              "cost=", "{:.9f}".format(c))
+
+print("Optimization Finished!")
+
+#Now, let's apply encoder and decoder for our tests.
+# Applying encode and decode over test set
+encode_decode = sess.run(
+    y_pred, feed_dict={X: mnist.test.images[:examples_to_show]})
+
+#Let's simply visualize our graphs!---
+# Compare original images with their reconstructions
+f, a = plt.subplots(2, 10, figsize=(10, 2))
+for i in range(examples_to_show):
+    a[0][i].imshow(np.reshape(mnist.test.images[i], (28, 28)))
+    a[1][i].imshow(np.reshape(encode_decode[i], (28, 28)))
+
